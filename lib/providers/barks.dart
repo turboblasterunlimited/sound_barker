a29aea1e7e9815e79c7066a5a6dd94fff4cbef34
handofthecode <deartovi@gmail.com>
2020-02-10 23:08:02 -0800

    extract Bark class

--- lib/providers/barks.dart
+++ lib/providers/barks.dart
@@ -1,8 +1,33 @@
-// class Bark {
-//   final String fileUrl;
-//   final String filePath;
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:uuid/uuid.dart';
+// import 'package:path_provider/path_provider.dart';
+import 'package:gcloud/storage.dart';
+import 'package:googleapis_auth/auth_io.dart' as auth;
+import 'dart:io';
 
-//   Bark({this.filePath, this.fileUrl});
-// }
+class Bark {
+  String fileUrl;
+  final String filePath;
+  final String uniqueFileName = Uuid().v4();
+  
+  Bark(this.filePath);
 
+  void uploadBark() {
+    rootBundle
+        .loadString('credentials/gcloud_credentials.json')
+        .then((credData) {
+      var credentials = auth.ServiceAccountCredentials.fromJson(credData);
+      List<String> scopes = []..addAll(Storage.SCOPES);
 
+      auth
+          .clientViaServiceAccount(credentials, scopes)
+          .then((auth.AutoRefreshingAuthClient client) {
+        var storage = Storage(client, 'songbarker');
+        Bucket bucket = storage.bucket('song_barker_sequences');
+        File(this.filePath)
+            .openRead()
+            .pipe(bucket.write("${this.uniqueFileName}.aac"));
+      });
+    });
+  }
+}
