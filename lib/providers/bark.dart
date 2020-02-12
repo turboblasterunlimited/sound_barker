fa8a2b97c142c2094cd85665e9a7399f9cde431d
handofthecode <deartovi@gmail.com>
2020-02-12 11:48:32 -0800

    put file in a folder in the bucket.

--- lib/providers/bark.dart
+++ lib/providers/bark.dart
@@ -5,6 +5,7 @@ import 'package:googleapis_auth/auth_io.dart' as auth;
 import 'dart:io';
 import 'package:random_string/random_string.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Bark with ChangeNotifier {
   String _title;
@@ -36,14 +37,31 @@ class Bark with ChangeNotifier {
     var info;
     Bucket bucket = await accessBucket();
     try {
-    ObjectInfo info = await File(this.filePath)
+    info = await File(this.filePath)
         .openRead()
-        .pipe(bucket.write("${this.uniqueFileName}.aac"));
+        .pipe(bucket.write("${this.uniqueFileName}/raw.aac"));
     } catch(error) {
       print('failed to put bark in the bucket');
       return error;
     }
     print(info.downloadLink);
+    await notifyServer();
     return this;
+  }
+
+    Future<void> notifyServer() async {
+    http.Response response;
+    // User ID hardcoded as 999 for now. This should be a post request in the future.
+    final url = 'http://165.227.178.14/999/${this.uniqueFileName}';
+    try {
+      response = await http.get(
+        url,
+      );
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw error;
+    }
+    print(response.body.toString());
   }
 }
