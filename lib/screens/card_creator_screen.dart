07e7fc8bd4cb35b8789ee14062cfed76f778499c
handofthecode <deartovi@gmail.com>
2020-05-14 21:46:37 -0700

    testing new webhook

--- lib/screens/card_creator_screen.dart
+++ lib/screens/card_creator_screen.dart
@@ -58,6 +58,7 @@ class _CardCreatorScreenState extends State<CardCreatorScreen> {
               child: Column(),
             ),
             // child: CardFinalizer(),
+            
           ),
         ],
       ),
