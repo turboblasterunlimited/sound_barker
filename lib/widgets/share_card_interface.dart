ed77ae844493a8b8695420501f4be94bbec376fe
handofthecode <deartovi@gmail.com>
2020-12-02 18:54:10 -0800

    fix share dialog overflow on smaller devices

--- lib/widgets/share_card_interface.dart
+++ lib/widgets/share_card_interface.dart
@@ -176,152 +176,156 @@ class _ShareCardInterfaceState extends State<ShareCardInterface> {
         builder: (ctx) {
           return StatefulBuilder(
               builder: (BuildContext context, Function setDialogState) {
-            return AlertDialog(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0))),
-              contentPadding: EdgeInsets.only(top: 10.0),
-              content: Container(
-                width: 300.0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Stack(
-                      children: [
-                        Positioned(
-                          right: 20,
-                          bottom: 5,
-                          child: Icon(
-                            CustomIcons.modal_share,
-                            size: 42,
-                            color: Colors.grey[300],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Center(
-                            child: Text(
-                              "Sharing is Caring",
-                              style: TextStyle(
-                                  color: Theme.of(context).primaryColor,
-                                  fontSize: 20),
+            return SingleChildScrollView(
+              child: AlertDialog(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0))),
+                contentPadding: EdgeInsets.only(top: 10.0),
+                content: Container(
+                  width: 300.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Stack(
+                        children: [
+                          Positioned(
+                            right: 20,
+                            bottom: 5,
+                            child: Icon(
+                              CustomIcons.modal_share,
+                              size: 42,
+                              color: Colors.grey[300],
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Divider(
-                      color: Colors.grey[300],
-                      thickness: 2,
-                    ),
-                    Column(
-                      children: [
-                        _loadingMessage == null
-                            ? Column(
-                                children: [
-                                  Center(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(12.0),
-                                      child: Text(
-                                        "Send card to",
-                                        style: TextStyle(
-                                            color:
-                                                Theme.of(context).primaryColor),
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(
-                                        left: 30.0, right: 30.0),
-                                    child: TextField(
-                                      onChanged: (name) {
-                                        recipientName = name;
-                                      },
-                                      onSubmitted: (_) async {
-                                        await _handleUploadAndShare(
-                                            setDialogState);
-                                      },
-                                      decoration: InputDecoration(
-                                        filled: true,
-                                        fillColor: Colors.white,
-                                        border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(16),
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Center(
+                              child: Text(
+                                "Sharing is Caring",
+                                style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        color: Colors.grey[300],
+                        thickness: 2,
+                      ),
+                      Column(
+                        children: [
+                          _loadingMessage == null
+                              ? Column(
+                                  children: [
+                                    Center(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(12.0),
+                                        child: Text(
+                                          "Send card to",
+                                          style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .primaryColor),
                                         ),
-                                        labelText: 'Recipient Name',
                                       ),
                                     ),
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                          left: 30.0, right: 30.0),
+                                      child: TextField(
+                                        onChanged: (name) {
+                                          recipientName = name;
+                                        },
+                                        onSubmitted: (_) async {
+                                          await _handleUploadAndShare(
+                                              setDialogState);
+                                        },
+                                        decoration: InputDecoration(
+                                          filled: true,
+                                          fillColor: Colors.white,
+                                          border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(16),
+                                          ),
+                                          labelText: 'Recipient Name',
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              : _loading(),
+                          Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(18),
+                                child: Icon(
+                                  CustomIcons.modal_paws_bottomright,
+                                  size: 42,
+                                  color: Colors.grey[300],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: InkWell(
+                              onTap: _loadingMessage != null
+                                  ? null
+                                  : () => _shareToClipboard(setDialogState),
+                              child: Container(
+                                padding:
+                                    EdgeInsets.only(top: 20.0, bottom: 20.0),
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(32.0),
                                   ),
-                                ],
-                              )
-                            : _loading(),
-                        Row(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.all(18),
-                              child: Icon(
-                                CustomIcons.modal_paws_bottomright,
-                                size: 42,
-                                color: Colors.grey[300],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: InkWell(
-                            onTap: _loadingMessage != null
-                                ? null
-                                : () => _shareToClipboard(setDialogState),
-                            child: Container(
-                              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                              decoration: BoxDecoration(
-                                color: Theme.of(context).primaryColor,
-                                borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(32.0),
                                 ),
-                              ),
-                              child: Text(
-                                "CLIPBOARD",
-                                style: TextStyle(color: Colors.white),
-                                textAlign: TextAlign.center,
+                                child: Text(
+                                  "CLIPBOARD",
+                                  style: TextStyle(color: Colors.white),
+                                  textAlign: TextAlign.center,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Container(
-                          width: 1,
-                        ),
-                        Expanded(
-                          child: InkWell(
-                            onTap: _loadingMessage != null
-                                ? null
-                                : () async {
-                                    _handleUploadAndShare(setDialogState);
-                                  },
-                            child: Container(
-                              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                              decoration: BoxDecoration(
-                                color: Theme.of(context).primaryColor,
-                                borderRadius: BorderRadius.only(
-                                  bottomRight: Radius.circular(32.0),
+                          Container(
+                            width: 1,
+                          ),
+                          Expanded(
+                            child: InkWell(
+                              onTap: _loadingMessage != null
+                                  ? null
+                                  : () async {
+                                      _handleUploadAndShare(setDialogState);
+                                    },
+                              child: Container(
+                                padding:
+                                    EdgeInsets.only(top: 20.0, bottom: 20.0),
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius: BorderRadius.only(
+                                    bottomRight: Radius.circular(32.0),
+                                  ),
                                 ),
-                              ),
-                              child: Text(
-                                "SHARE",
-                                style: TextStyle(color: Colors.white),
-                                textAlign: TextAlign.center,
+                                child: Text(
+                                  "SHARE",
+                                  style: TextStyle(color: Colors.white),
+                                  textAlign: TextAlign.center,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
@@ -338,16 +342,16 @@ class _ShareCardInterfaceState extends State<ShareCardInterface> {
 
   Future<String> _handleUpload(Function setDialogState) async {
     try {
-    if (!cards.current.audio.exists)
-      showError(context, "Card has no audio");
-    else if (_editingCard()) {
-      print("editing card");
-      return await _updateCard(setDialogState);
-    } else {
-      print("creating new card");
-      return await _createCard(setDialogState);
-    }
-    } catch(e) {
+      if (!cards.current.audio.exists)
+        showError(context, "Card has no audio");
+      else if (_editingCard()) {
+        print("editing card");
+        return await _updateCard(setDialogState);
+      } else {
+        print("creating new card");
+        return await _createCard(setDialogState);
+      }
+    } catch (e) {
       print("upload card error: $e");
       showError(context, e);
     }
