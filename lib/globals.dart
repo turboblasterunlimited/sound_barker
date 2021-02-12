a230d32e89624f610dbcd388019c7e8849140257
handofthecode <deartovi@gmail.com>
2021-02-11 19:58:41 -0800

    adjust default mouth position

--- lib/globals.dart
+++ lib/globals.dart
@@ -12,9 +12,9 @@ const String bucketName = 'song_barker_sequences';
 const Map<String, dynamic> defaultFaceCoordinates = {
   "leftEye": [-0.15, 0.2],
   "rightEye": [0.15, 0.2],
-  "mouth": [0.0, 0.0],
-  "mouthLeft": [-0.1, 0.0],
-  "mouthRight": [0.1, 0.0],
+  "mouth": [0.0, -0.15],
+  "mouthLeft": [-0.1, -0.1],
+  "mouthRight": [0.1, -0.1],
   "headTop": [0.0, 0.4],
   "headRight": [0.3, 0.0],
   "headBottom": [0.0, -0.4],
