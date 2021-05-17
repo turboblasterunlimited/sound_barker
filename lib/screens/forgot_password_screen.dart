48432ea63a1ae03e504e13c810b9109ece5b1c76
Jeff Fried <clevcollc@gmail.com>
2021-05-17 15:22:20 -0700

    added Forgot Password

---
+++ lib/screens/forgot_password_screen.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+import 'package:K9_Karaoke/icons/custom_icons.dart';
+import 'package:K9_Karaoke/providers/the_user.dart';
+import 'package:K9_Karaoke/screens/retrieve_data_screen.dart';
+import 'package:K9_Karaoke/services/rest_api.dart';
+import 'package:K9_Karaoke/widgets/custom_dialog.dart';
+import 'package:K9_Karaoke/widgets/error_dialog.dart';
+import 'package:K9_Karaoke/widgets/user_agreement.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:line_awesome_icons/line_awesome_icons.dart';
+import 'package:openid_client/openid_client_io.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+
+import 'package:K9_Karaoke/services/http_controller.dart';
+import 'package:provider/provider.dart';
+import '../services/authenticate_user.dart';
+import 'package:K9_Karaoke/globals.dart';
+
+class ForgotPasswordDialog extends StatefulWidget {
+  static const routeName = 'forgot-password-screen';
+  @override
+  _ForgotPasswordDialogState createState() => _ForgotPasswordDialogState();
+}
+
+class _ForgotPasswordDialogState extends State<ForgotPasswordDialog> {
+  TextEditingController _textFieldController = TextEditingController();
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('TextField in Dialog'),
+            content: TextField(
+              onChanged: (value) {
+                setState(() {
+                  valueText = value;
+                });
+              },
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: "Text Field in Dialog"),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                child: Text('CANCEL'),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                textColor: Colors.white,
+                child: Text('OK'),
+                onPressed: () {
+                  setState(() {
+                    codeDialog = valueText;
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  String codeDialog;
+  String valueText;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: (codeDialog == "123456") ? Colors.green : Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text('Alert Dialog'),
+      ),
+      body: Center(
+        child: FlatButton(
+          color: Colors.teal,
+          textColor: Colors.white,
+          onPressed: () {
+            _displayTextInputDialog(context);
+          },
+          child: Text('Press For Alert'),
+        ),
+      ),
+    );
+  }
+}
