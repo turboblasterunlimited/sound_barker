0a471cfab65812213f9679cac99c04a6b2819f62
turboblasterunlimited <66650587+turboblasterunlimited@users.noreply.github.com>
2021-07-29 20:40:39 -0700

    Added links to facebook and instagram sites in the icon buttons.

--- lib/widgets/social_links_bar.dart
+++ lib/widgets/social_links_bar.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'package:url_launcher/url_launcher.dart';
+
+const faceBookURL = "https://www.facebook.com/K9Karaoke1/";
+const instagramURL = "https://www.instagram.com/ribbond_dental/";
+
+void _launchFacebook() async => await canLaunch(faceBookURL)
+    ? await launch(faceBookURL)
+    : throw 'Could not launch $faceBookURL';
+
+void _launchInstagram() async => await canLaunch(instagramURL)
+    ? await launch(instagramURL)
+    : throw 'Could not launch $instagramURL';
+
 Widget socialLinksBar(BuildContext context) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.center,
     crossAxisAlignment: CrossAxisAlignment.center,
     children: <Widget>[
       RawMaterialButton(
-        onPressed: null,
+        onPressed: _launchFacebook,
         child: Image.asset(
           "assets/logos/facebook256.png",
           height: 40,
@@ -17,7 +30,7 @@ Widget socialLinksBar(BuildContext context) {
         padding: const EdgeInsets.all(10.0),
       ),
       RawMaterialButton(
-        onPressed: null,
+        onPressed: _launchInstagram,
         child: Image.asset(
           "assets/logos/Instagram_AppIcon_Aug2017.png",
           height: 40,
